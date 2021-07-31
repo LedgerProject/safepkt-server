5373a3c4b75f3cb025a835a4befd11ac5bcbebf5
Thierry Marianne <thierry@marianne.io>
2021-07-31 10:19:40 +0200

    Remove exec call

--- src/infrastructure/docker/client.rs
+++ src/infrastructure/docker/client.rs
@@ -3,15 +3,12 @@ use anyhow::Result;
 use bollard::container::{
     Config, CreateContainerOptions, ListContainersOptions, RemoveContainerOptions,
 };
-use bollard::exec::{CreateExecOptions, StartExecResults};
 use bollard::models::*;
 use bollard::Docker;
 use color_eyre::Report;
-use futures::StreamExt;
 use std::collections::HashMap;
 use std::default::Default;
 use std::env;
-use tracing::info;
 
 static TARGET_RVT_DIRECTORY: &str = "/home/rust-verification-tools";
 static TARGET_SOURCE_DIRECTORY: &str = "/source";
@@ -56,13 +53,32 @@ async fn remove_existing_container(docker: &Docker, source_hash: &str) -> Result
     Ok(())
 }
 
+fn get_verification_command<'a>(
+    source_hash: &'a str,
+    bitcode: &'a str,
+    mut command: Vec<&'a str>,
+) -> Vec<&'a str> {
+    command.push("cargo");
+    command.push("verify");
+    command.push("-v");
+    command.push("--bin");
+    command.push(source_hash);
+    command.push("-o");
+    command.push(bitcode);
+
+    command
+}
+
 fn get_configuration<'a>(
     source_hash: &'a str,
     docker_image: &'a str,
+    prefixed_hash: &'a str,
+    bitcode: &'a str,
 ) -> Result<Config<&'a str>, Report> {
     let rvt_directory = get_rvt_directory()?;
 
     let host_config = HostConfig {
+        auto_remove: Some(false),
         mounts: Some(vec![
             Mount {
                 target: Some(TARGET_SOURCE_DIRECTORY.to_string()),
@@ -83,10 +99,14 @@ fn get_configuration<'a>(
         ..Default::default()
     };
 
+    let command = Vec::<&'a str>::new();
+    let command = get_verification_command(prefixed_hash, bitcode, command);
+
     Ok(Config {
-        entrypoint: Some(vec!["tail", "-f", "/dev/null"]),
+        cmd: Some(command),
         host_config: Some(host_config),
         image: Some(docker_image),
+        user: Some("1000:1000"),
         working_dir: Some(TARGET_SOURCE_DIRECTORY),
         ..Default::default()
     })
@@ -99,7 +119,15 @@ async fn start_static_analysis_container<'a>(
     remove_existing_container(docker, source_hash).await?;
 
     let docker_image = get_rvt_docker_image()?;
-    let configuration = get_configuration(source_hash.into(), &docker_image.as_str())?;
+
+    let prefixed_hash = prefix_hash(source_hash);
+    let bitcode = format!("{}.bc", source_hash);
+    let configuration = get_configuration(
+        source_hash.into(),
+        &docker_image.as_str(),
+        prefixed_hash.as_str(),
+        bitcode.as_str(),
+    )?;
 
     let id = docker
         .create_container(
@@ -117,38 +145,7 @@ async fn start_static_analysis_container<'a>(
 pub async fn run_static_analysis(source_hash: &str) -> Result<(), Report> {
     let docker = Docker::connect_with_socket_defaults()?;
 
-    let (docker, id) = start_static_analysis_container(&docker, source_hash).await?;
+    start_static_analysis_container(&docker, source_hash).await?;
 
-    let prefixed_source_hash = prefix_hash(source_hash);
-
-    let exec = docker
-        .create_exec(
-            &id,
-            CreateExecOptions {
-                attach_stdout: Some(true),
-                attach_stderr: Some(true),
-                cmd: Some(vec![
-                    "cargo",
-                    "verify",
-                    "-v",
-                    "--bin",
-                    prefixed_source_hash.as_str(),
-                    "-o",
-                    format!("{}.bc", prefixed_source_hash).as_str(),
-                ]),
-                ..Default::default()
-            },
-        )
-        .await?
-        .id;
-
-    if let StartExecResults::Attached { mut output, .. } = docker.start_exec(&exec, None).await? {
-        while let Some(Ok(msg)) = output.next().await {
-            info!("{}", msg);
-        }
-
-        return Ok(());
-    }
-
-    unreachable!();
+    Ok(())
 }
