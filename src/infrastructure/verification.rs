4b94fa8576c456746c6ecdb04845f9def55df7d4
Thierry Marianne <thierry@marianne.io>
2021-08-01 15:27:40 +0200

    Move verification implementation and trait to runtime module

--- src/infrastructure/verification.rs
+++ src/infrastructure/verification.rs
@@ -1,1 +1,52 @@
 pub mod runtime;
+
+use crate::domain::verification::service::runtime as verification_runtime;
+use async_trait::async_trait;
+use bollard::Docker;
+use color_eyre::Report;
+use runtime::docker::container;
+use runtime::docker::ContainerAPIClient;
+use verification_runtime::LLVMBitcodeGenerator;
+use verification_runtime::VerificationRuntime;
+
+impl VerificationRuntime<ContainerAPIClient<Docker>> {
+    pub fn new(target_hash: &'_ str) -> Result<Self, Report> {
+        let container_api_client = ContainerAPIClient::new().unwrap();
+
+        Ok(VerificationRuntime {
+            container_api_client,
+            target_hash: String::from(target_hash),
+        })
+    }
+
+    pub fn container_api_client(&self) -> &ContainerAPIClient<Docker> {
+        &self.container_api_client
+    }
+
+    pub fn target_hash(&self) -> &str {
+        self.target_hash.as_str()
+    }
+
+    async fn remove_existing_container(&self) -> Result<(), Report> {
+        container::remove_existing_container(self.container_api_client(), self.target_hash())
+            .await?;
+
+        Ok(())
+    }
+
+    async fn start_rvt_container(&self) -> Result<(), Report> {
+        container::start_container(self.container_api_client(), self.target_hash()).await?;
+
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl LLVMBitcodeGenerator<Result<(), Report>> for VerificationRuntime<ContainerAPIClient<Docker>> {
+    async fn start_llvm_bitcode_generation(&self) -> Result<(), Report> {
+        self.remove_existing_container().await?;
+        self.start_rvt_container().await?;
+
+        Ok(())
+    }
+}
--- src/infrastructure/verification/runtime/docker.rs
+++ src/infrastructure/verification/runtime/docker.rs
@@ -1,54 +1,9 @@
 pub mod container;
 
-use crate::domain::verification::service::runtime as verification_runtime;
 use anyhow::Result;
 use async_trait::async_trait;
 use bollard::Docker;
 use color_eyre::Report;
-use verification_runtime::LLVMBitcodeGenerator;
-use verification_runtime::VerificationRuntime;
-
-impl VerificationRuntime<ContainerAPIClient<Docker>> {
-    pub fn new(target_hash: &'_ str) -> Result<Self, Report> {
-        let container_api_client = ContainerAPIClient::new().unwrap();
-
-        Ok(VerificationRuntime {
-            container_api_client,
-            target_hash: String::from(target_hash),
-        })
-    }
-
-    pub fn container_api_client(&self) -> &ContainerAPIClient<Docker> {
-        &self.container_api_client
-    }
-
-    pub fn target_hash(&self) -> &str {
-        self.target_hash.as_str()
-    }
-
-    async fn remove_existing_container(&self) -> Result<(), Report> {
-        container::remove_existing_container(self.container_api_client(), self.target_hash())
-            .await?;
-
-        Ok(())
-    }
-
-    async fn start_rvt_container(&self) -> Result<(), Report> {
-        container::start_container(self.container_api_client(), self.target_hash()).await?;
-
-        Ok(())
-    }
-}
-
-#[async_trait]
-impl LLVMBitcodeGenerator<Result<(), Report>> for VerificationRuntime<ContainerAPIClient<Docker>> {
-    async fn start_llvm_bitcode_generation(&self) -> Result<(), Report> {
-        self.remove_existing_container().await?;
-        self.start_rvt_container().await?;
-
-        Ok(())
-    }
-}
 
 pub struct ContainerAPIClient<C> {
     client: C,
