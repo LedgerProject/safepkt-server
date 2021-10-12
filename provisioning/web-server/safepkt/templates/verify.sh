bed271dcea891e813a56577a77438d2512531fa5
Thierry Marianne <thierry@marianne.io>
2021-10-12 11:28:57 +0400

    Make deps before running verification so that cargo caches downloaded dependencies at compile time persistently

--- provisioning/web-server/safepkt/templates/verify.sh
+++ provisioning/web-server/safepkt/templates/verify.sh
@@ -4,6 +4,16 @@ function verify() {
     local output
     output="${1}"
 
+    local cargo_home=
+    cargo_home=/ink/examples/source/deps
+
+    if [ ! -d "${cargo_home}" ];
+    then
+      mkdir "${cargo_home}"
+    fi
+
+    export CARGO_HOME="${cargo_home}"
+
     sudo chmod -R ug+rwx /ink/examples/source && \
     sudo chown -R 101:102 /ink/examples/source && \
     cp -R /ink/examples/erc20/.ink /ink/examples/source && \
