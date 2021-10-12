3958b13d00da6a662d91a9aa31c6ee4a3591a551
Thierry Marianne <thierry@marianne.io>
2021-10-12 11:25:29 +0400

    Build safepkt/rvt:verifier docker image

--- provisioning/web-server/safepkt/templates/verify.sh
+++ provisioning/web-server/safepkt/templates/verify.sh
@@ -4,6 +4,8 @@ function verify() {
     local output
     output="${1}"
 
+    sudo chmod -R ug+rwx /ink/examples/source && \
+    sudo chown -R 101:102 /ink/examples/source && \
     cp -R /ink/examples/erc20/.ink /ink/examples/source && \
     cargo verify --tests -v -o "${output}" && \
     klee --libc=klee --silent-klee-assume --warnings-only-to-file "${output}"
