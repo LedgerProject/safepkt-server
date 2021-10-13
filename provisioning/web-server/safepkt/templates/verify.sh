b780cd52e068d79e3c753268093d0270dbadeeed
Thierry Marianne <thierry@marianne.io>
2021-10-13 18:34:33 +0400

    Add safepkt- prefix to ink repository name

--- provisioning/web-server/safepkt/templates/verify.sh
+++ provisioning/web-server/safepkt/templates/verify.sh
@@ -11,7 +11,7 @@ function verify() {
     smart_contract_example="${3}"
 
     local cargo_home=
-    cargo_home='/ink/examples/source/deps'
+    cargo_home='/safepkt-ink/examples/source/deps'
 
     if [ ! -d "${cargo_home}" ];
     then
@@ -20,9 +20,9 @@ function verify() {
 
     export CARGO_HOME="${cargo_home}"
 
-    cp -R "/ink/examples/${smart_contract_example}/.ink" /ink/examples/source && \
-    sed -i 's/'"${smart_contract_example}"'/'"${package_name}"'/g' /ink/examples/source/.ink/abi_gen/Cargo.toml && \
-    sed -i 's/'"${smart_contract_example}"'/'"${package_name}"'/g' /ink/examples/source/src/lib.rs && \
+    cp -R "/safepkt-ink/examples/${smart_contract_example}/.ink" /safepkt-ink/examples/source && \
+    sed -i 's/'"${smart_contract_example}"'/'"${package_name}"'/g' /safepkt-ink/examples/source/.ink/abi_gen/Cargo.toml && \
+    sed -i 's/'"${smart_contract_example}"'/'"${package_name}"'/g' /safepkt-ink/examples/source/src/lib.rs && \
     cargo verify --script=./commands.sh --tests -v -o "${output}" && \
     klee --libc=klee --silent-klee-assume --warnings-only-to-file "${output}"
 }
