2f54f55dff23c52981fdfc7440cdc76093d2571f
Thierry Marianne <thierry@marianne.io>
2021-10-14 15:24:34 +0400

    Add line feeds to setup_verifier function body

--- provisioning/web-server/safepkt/templates/setup-verifier.sh
+++ provisioning/web-server/safepkt/templates/setup-verifier.sh
@@ -8,14 +8,19 @@ function setup_verifier() {
   workdir="$(pwd)"
 
   apt install -y vim less gdb htop
+
   git clone https://github.com/thierrymarianne/safepkt-arrayvec /safepkt-arrayvec
   cd /safepkt-arrayvec && git checkout 0.4.13
+
   git clone https://github.com/thierrymarianne/safepkt-rand /safepkt-rand
   cd /safepkt-rand && git checkout 0.7.4
+
   git clone https://github.com/paritytech/ink /safepkt-ink
   cd /safepkt-ink && git checkout v2.1.0
+
   git clone https://github.com/thierrymarianne/safepkt-rust-smallvec /safepkt-rust-smallvec
   cd /safepkt-rust-smallvec && git checkout v1.7.1
+
   chown -R ${UID_GID} /safepkt-ink
   chmod -R ug+rwx /safepkt-ink
   chmod a+x /usr/local/bin/verify
