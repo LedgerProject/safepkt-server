f43ca72c7cbbf3701ab9a0f807809baffe427587
Thierry Marianne <thierry@marianne.io>
2021-10-15 11:44:33 +0400

    Prevent original smart contract name from being replaced with generated package id

--- provisioning/web-server/safepkt/templates/verify.sh
+++ provisioning/web-server/safepkt/templates/verify.sh
@@ -32,6 +32,10 @@ function verify() {
     cp -R /safepkt-runtime /home/rust-verification-tools/runtime && \
       echo '=> Successfully copied LLVM bitcode generated from RVT ./runtime' >> /safepkt-ink/examples/source/verification.log
 
+    cp -R "/safepkt-ink/examples/${smart_contract_example}/.ink" /safepkt-ink/examples/source && \
+    sed -i 's/'"${smart_contract_example}"'/'"${package_name}"'/g' /safepkt-ink/examples/source/.ink/abi_gen/Cargo.toml && \
+    sed -i 's/'"${smart_contract_example}"'/'"${package_name}"'/g' /safepkt-ink/examples/source/src/lib.rs
+
     if [ "$(grep -c '"'"project_name"'"' /safepkt-ink/examples/source/src/lib.rs)" == "0" ];
     then
       echo "" >> /safepkt-ink/examples/source/src/lib.rs && \
@@ -39,9 +43,6 @@ function verify() {
       echo "" >> /safepkt-ink/examples/source/src/lib.rs
     fi
 
-    cp -R "/safepkt-ink/examples/${smart_contract_example}/.ink" /safepkt-ink/examples/source && \
-    sed -i 's/'"${smart_contract_example}"'/'"${package_name}"'/g' /safepkt-ink/examples/source/.ink/abi_gen/Cargo.toml && \
-    sed -i 's/'"${smart_contract_example}"'/'"${package_name}"'/g' /safepkt-ink/examples/source/src/lib.rs && \
     cargo verify --backend=klee --script=./commands.sh --tests
 }
 verify "${1}" "${2}" ${3}
