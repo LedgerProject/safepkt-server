8856f0429348303ca8b54ecdf406a7fff1c040ab
Thierry Marianne <thierry@marianne.io>
2021-10-18 14:58:25 +0400

    Remove vuex-persist plugin

--- plugins/vuex-persist.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-named-as-default
-import VuexPersistence from 'vuex-persist'
-
-export default ({ store }: any) => {
-  new VuexPersistence({
-    storage: window.localStorage
-  }).plugin(store)
-}
